828531a61454135f1eb525bf4bb233c38da7d344
hocle <hoc.le@oivan.com>
2024-07-10 16:08:34 +0700

    ::Jobs::Base

--- jobs/update_secure_status_job.rb
+++ jobs/update_secure_status_job.rb
@@ -1,16 +1,15 @@
 module Jobs
-    class UpdateSecureStatusJob < ActiveJob::Base
-      def perform(args)
+    class UpdateSecureStatusJob < ::Jobs::Base
+      def execute(args)
         topic_id = args[:topic_id]
         topic = Topic.find_by(id: topic_id)
         return unless topic
   
         begin
-          topic.image_upload.update_secure_status(override: false)
-        rescue => e
-          Rails.logger.error("Cannot update image secure status for topic ID #{topic.id}: #{e.message}")
+            topic.image_upload.update_secure_status(override: false)
+        rescue
+            Rails.logger.error("Cannot update image sercue: #{topic.id}")
         end
       end
     end
   end
-  
