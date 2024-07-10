ebff55de6bf73dca77b0fbaead76559aa3e3efdb
hocle <hoc.le@oivan.com>
2024-07-10 15:37:47 +0700

    update require_relative

--- plugin.rb
+++ plugin.rb
@@ -3,13 +3,16 @@
 # version: 0.1
 # authors: Hoc Le
 
+# Require the job file
+require_relative 'jobs/update_secure_status_job'
+
 after_initialize do
     DiscourseEvent.on(:topic_created) do |topic, opts, user|
         next if topic.nil?
 
         Rails.logger.info("Edit topic created with ID: #{topic.id}")
         # Enqueue job to update secure status after 30 seconds
-        ::Jobs.enqueue_in(30.seconds, :update_secure_status_job, topic_id: topic.id)
+        Jobs.enqueue_in(30.seconds, :update_secure_status_job, topic_id: topic.id)
     end
 
     DiscourseEvent.on(:topic_edited) do |topic, opts, user|
@@ -17,6 +20,6 @@ after_initialize do
   
         Rails.logger.info("Edit topic created with ID: #{topic.id}")
         # Enqueue job to update secure status after 30 seconds
-        ::Jobs.enqueue_in(30.seconds, :update_secure_status_job, topic_id: topic.id)
+        Jobs.enqueue_in(30.seconds, :update_secure_status_job, topic_id: topic.id)
     end
 end
