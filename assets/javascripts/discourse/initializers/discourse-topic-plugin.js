f722f4dd66c0b9772af03eefbcbc60ed3c167d2d
hocle <hoc.le@oivan.com>
2024-07-18 23:18:29 +0700

    add assets_dir

--- assets/javascripts/discourse/initializers/discourse-topic-plugin.js
+++ assets/javascripts/discourse/initializers/discourse-topic-plugin.js
@@ -1,27 +1,24 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-function initializeVideoAutoplay(api) {
-  console.log('initializeVideoAutoplay');
-  api.onPageChange(() => {
-    console.log('onPageChange');
-    document.querySelectorAll('video').forEach(video => {
-      console.log('video', video);
-      // Set video to autoplay, mute, and loop
-      video.setAttribute('autoplay', true);
-      video.setAttribute('muted', true);
-      video.setAttribute('loop', true);
-
-      // Disable right-click context menu
-      video.addEventListener('contextmenu', function(e) {
-        e.preventDefault();
-      });
-    });
-  });
-}
-
 export default {
   name: 'discourse-topic-plugin',
   initialize() {
-    withPluginApi('0.8.32', (api) => initializeVideoAutoplay(api));
+    withPluginApi('0.8.32', (api) => {
+      api.onPageChange(() => {
+        console.log('onPageChange');
+        document.querySelectorAll('video').forEach(video => {
+          console.log('video', video);
+          // Set video to autoplay, mute, and loop
+          video.setAttribute('autoplay', true);
+          video.setAttribute('muted', true);
+          video.setAttribute('loop', true);
+    
+          // Disable right-click context menu
+          video.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+          });
+        });
+      });
+    });
   }
 };
