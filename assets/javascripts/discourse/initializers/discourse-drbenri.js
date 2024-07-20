cb0f0c068c651e1bb7ed5335e96a2af6d2233ea7
hocle <hoc.le@oivan.com>
2024-07-20 18:34:28 +0700

    function initializePlugin

--- assets/javascripts/discourse/initializers/discourse-drbenri.js
+++ assets/javascripts/discourse/initializers/discourse-drbenri.js
@@ -3,8 +3,20 @@ import { withPluginApi } from 'discourse/lib/plugin-api';
 function initializePlugin(api)
 {
   api.onPageChange((url, title) => {
-        // your code here
-        console.log('the page changed to: ' + url + ' and title ' + title);
+        //topic page url: /t/2016-olympics-what-rio-doesn-t-want-the-world-to-see/38
+        const topicUrl = url.match(/\/t\/([^\/]+)\/(\d+)/);
+        if (topicUrl) {
+          //autoplay videos and disable right click to video
+          document.addEventListener('DOMContentLoaded', function() {
+            //autoplay videos
+            const videos = document.querySelectorAll('video');
+            videos.forEach(video => video.muted = true);
+            //disable right click on videos
+            document.addEventListener('contextmenu', function(event) {
+              event.preventDefault();
+            });
+          });
+        }
   });
 }
 
