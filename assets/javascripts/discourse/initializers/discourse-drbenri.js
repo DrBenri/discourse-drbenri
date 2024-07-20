ad1398a779db7eaf46c8c8be4da237f203fa697b
hocle <hoc.le@oivan.com>
2024-07-20 21:54:55 +0700

    Update siteSettings

--- assets/javascripts/discourse/initializers/discourse-drbenri.js
+++ assets/javascripts/discourse/initializers/discourse-drbenri.js
@@ -7,23 +7,19 @@ function initializePlugin(api)
         const topicUrl = url.match(/\/t\/([^\/]+)\/(\d+)/);
         if (topicUrl) {
           // check app settings for plugin enabled
-          const settings = api.container.lookup('site-settings:main');
-          const pluginEnabled = settings.get('drbenri_enabled');
-          if (!pluginEnabled) return;
+          const siteSettings = api.container.lookup("service:site-settings");
+          if (!siteSettings.drbenri_enabled) return;
 
           // check settings for autoplay: auto_play_video
           // play video on topic page
-          const autoPlayVideo = settings.get('auto_play_video');
-          if (autoPlayVideo) {
+          if (settings.auto_play_video) {
             const videoElement = document.querySelector(".video-placeholder-container");
             videoElement.click();
           }
 
           // check settings for disable right click: disable_video_controls
           // disable video controls on topic page
-          const disableVideoControls = settings.get('disable_video_controls');
-          if (disableVideoControls) {
-            const videos = document.querySelectorAll('video');
+          if (settings.disable_video_controls) {
             setTimeout(() => {
               const videos = document.querySelectorAll('video');
               
