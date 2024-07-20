0a5c2c8d3b8cf04e2dd687b328090ccc05b11260
hocle <hoc.le@oivan.com>
2024-07-20 21:22:23 +0700

    Update setting

--- assets/javascripts/discourse/initializers/discourse-drbenri.js
+++ assets/javascripts/discourse/initializers/discourse-drbenri.js
@@ -6,34 +6,44 @@ function initializePlugin(api)
         //topic page url: /t/2016-olympics-what-rio-doesn-t-want-the-world-to-see/38
         const topicUrl = url.match(/\/t\/([^\/]+)\/(\d+)/);
         if (topicUrl) {
-          //play video on topic page
-          const videoElement = document.querySelector(".video-placeholder-container");
-          videoElement.click();
+          // check app settings for plugin enabled
+          const settings = api.container.lookup('site-settings:main');
+          const pluginEnabled = settings.get('drbenri_enabled');
+          if (!pluginEnabled) return;
 
-          setTimeout(() => {
+          // check settings for autoplay: auto_play_video
+          // play video on topic page
+          const autoPlayVideo = settings.get('auto_play_video');
+          if (autoPlayVideo) {
+            const videoElement = document.querySelector(".video-placeholder-container");
+            videoElement.click();
+          }
+
+          // check settings for disable right click: disable_video_controls
+          // disable video controls on topic page
+          const disableVideoControls = settings.get('disable_video_controls');
+          if (disableVideoControls) {
             const videos = document.querySelectorAll('video');
-            
-            videos.forEach(video => {
-              video.muted = true;
-              video.controlsList.add('nodownload');
-              video.oncontextmenu = function() {
-                return false;
-              };
-            });
-
-            // Disable right-click on videos only
-            document.addEventListener('contextmenu', function(event) {
-              if (event.target.tagName === 'VIDEO') {
-                event.preventDefault();
-              }
-            });
-          }, 300);
-      } else {
-        // enable right click on videos
-        document.removeEventListener('contextmenu', function(event) {
-          event.preventDefault();
-        });
-      }
+            setTimeout(() => {
+              const videos = document.querySelectorAll('video');
+              
+              videos.forEach(video => {
+                video.muted = true;
+                video.controlsList.add('nodownload');
+                video.oncontextmenu = function() {
+                  return false;
+                };
+              });
+  
+              // Disable right-click on videos only
+              document.addEventListener('contextmenu', function(event) {
+                if (event.target.tagName === 'VIDEO') {
+                  event.preventDefault();
+                }
+              });
+            }, 100);
+          }
+      };
   });
 }
 
