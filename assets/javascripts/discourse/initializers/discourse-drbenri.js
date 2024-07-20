506c7c1a58cb880b1556512a2cc4f65421265382
hocle <hoc.le@oivan.com>
2024-07-20 19:10:51 +0700

    Add log

--- assets/javascripts/discourse/initializers/discourse-drbenri.js
+++ assets/javascripts/discourse/initializers/discourse-drbenri.js
@@ -5,18 +5,24 @@ function initializePlugin(api)
   api.onPageChange((url, title) => {
         //topic page url: /t/2016-olympics-what-rio-doesn-t-want-the-world-to-see/38
         const topicUrl = url.match(/\/t\/([^\/]+)\/(\d+)/);
+        console.log(topicUrl);
         if (topicUrl) {
-          //autoplay videos and disable right click to video
-          document.addEventListener('DOMContentLoaded', function() {
+          //play video on topic page
+          const videoElement = document.querySelector(".video-placeholder-container");
+          log(videoElement);
+          videoElement.click();
+
+          setTimeout(() => {
             //autoplay videos
             const videos = document.querySelectorAll('video');
+            log(videos);
             videos.forEach(video => video.muted = true);
             //disable right click on videos
             document.addEventListener('contextmenu', function(event) {
               event.preventDefault();
             });
-          });
-        }
+          }, 300);
+      }
   });
 }
 
